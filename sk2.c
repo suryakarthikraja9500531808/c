47c22593293eac01d9ad6a34304a684c7115f3da
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-01-31 14:29:25 +0530

    Create sk2.c

---
+++ sk2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the values of a");
+scanf("%d",&a);
+if(number % 2==0);
+printf("%d is even",number);
+else
+printf("%d is odd",number);
+getch();
+}
