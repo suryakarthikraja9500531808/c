45b6caa7e594ab4fe17dc29312ea9d040330ce99
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-02-09 16:00:48 +0530

    Create karthikraja20.c

---
+++ karthikraja20.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include<conio.h>
+void main()
+
+{
+
+    int num,m =20,n=40;
+
+    clrscr();
+
+    printf("Print Odd Numbers in a given range m to n:\n");
+
+    for (num = m; num <= n; num++)
+
+        {
+
+               if (num % 2 == 1)
+
+                  printf ("%d ", num);
+
+         }
+
+                getch();
+                }
