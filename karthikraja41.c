8235f8904a1231b03d96b68fc5fe117824ea700c
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-02-12 13:33:32 +0530

    Create karthikraja41.c

---
+++ karthikraja41.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   char s[100];
+   int n,i;
+   scanf("%s",s);
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       printf("\n %s",s);
+      
+   }
+   
+    getch();
+}
