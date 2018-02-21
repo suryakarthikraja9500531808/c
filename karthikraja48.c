793c57895ef88cd189ae7dfd520623a1298ab05b
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-02-21 14:03:29 +0530

    Create karthikraja48.c

---
+++ karthikraja48.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+void  main()
+{
+	int a,i;
+	printf("enter the a value");
+	scanf("%d",&a);
+	for(i=1;i<a;i++)
+	{
+		printf("%d",i);
+	}
+	if=(a/2!=0)
+	{
+	printf("the average value is");
+	}
+	getch();
+}
