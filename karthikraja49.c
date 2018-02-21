ee6e80585474c9da994932b5ca4739cd4a0cd258
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-02-21 14:05:50 +0530

    Create karthikraja49.c

---
+++ karthikraja49.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+int a,b,c;
+printf("enter the a value");
+scanf("%d",&a);
+printf("enter the b value");
+scanf("%d",&b);
+c=a+b;
+printf("the value of c is");
+getch();
+}
