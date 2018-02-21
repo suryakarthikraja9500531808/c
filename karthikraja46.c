655f0413bbe336042d93b04ede995d436c47dcaf
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-02-21 13:58:39 +0530

    Create karthikraja46.c

---
+++ karthikraja46.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n<=10000000)
+    {
+    n++;
+    break;
+    }
+    printf("%d",n);
+    getch();
+}
