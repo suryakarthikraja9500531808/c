43c32a18540748e1896c790eb92ffc5b8b51a586
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-02-23 16:04:56 +0530

    Create karthikraja54.c

---
+++ karthikraja54.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+void main();
+{
+int n,digit,sum=0;
+scanf("%d",&n);
+             if(n%2==0)
+             {
+             	printf("%d",n);
+             }
+             else
+             {
+             	printf("%d",n-1);
+             }
+	getch();
+  }
