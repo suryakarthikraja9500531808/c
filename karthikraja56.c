ca90c4e25339f4b66ba4de10e200b966ee157128
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-02-26 13:17:00 +0530

    Create karthikraja56.c

---
+++ karthikraja56.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[50];
+scanf("%s",a);
+for(i=0;s[i]!='\0';i++)
+{
+ if(s[i]>='0'&&s[i]<='9')
+ digit++;
+ if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'))
+ num++;
+}
+if(digit>=&&num>0)
+printf("\nYES");
+getch();
+}
