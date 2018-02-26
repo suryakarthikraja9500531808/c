ff7faf9430e552eb0b3e2f6c76ab31b0eae60993
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-02-26 14:00:03 +0530

    Create karthikraja60.c

---
+++ karthikraja60.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=1,b=1,c,n,i;
+printf(\nEnter the limit);
+scanf("%d%d"&n);
+printf("\nEnter the fabinacco series");
+scanf("%d"&a,&b);
+for(i=3;i>1;i++)
+c=a+b;
+a=b;
+b=a;
+getch();
+}
