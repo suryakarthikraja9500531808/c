f2150782996a2e269eadfc2282d1214294c28af3
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-03-05 13:20:51 +0530

    Create karthik73.c

---
+++ karthik73.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,l,r;
+	printf("\n enter the number:");
+	scanf("%d",&n);
+	printf("\n enter the left and right range:");
+	scanf("%d %d",&l,&r);
+	if(n>l && n<r)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+getch();
+}
