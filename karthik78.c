ee6f1dc2117513501744057b96936b64a21d74cb
suryakarthikraja9500531808 <35688176+suryakarthikraja9500531808@users.noreply.github.com>
2018-03-07 14:03:30 +0530

    Create karthik78.c

---
+++ karthik78.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+
+{
+
+	int n;
+
+	printf("enter the number:");
+
+	scanf("%d",&n);
+
+	if(n%13==0)
+
+	{
+
+		printf("\n yes");
+
+	}
+
+	else
+
+	{
+
+		printf("\n no");
+
+	}
+
+	getch();
+
+}
